2b6b76ae44d35d9668e4f576d3d8358b78710f66
cpap7 <mjajarmi2000@outlook.com>
2026-02-26 19:39:56 -0500

    Reworked TTS config files into one monolithic file. Moved config file to Core folder

---
+++ VoxBox-PiperAPI/Source/Core/TTSConfig.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <optional>
+#include <filesystem>
+
+namespace VoxBox {
+    // Voice model paths
+    struct SVoiceModelConfig {
+        std::optional<std::filesystem::path> m_tashkeel_model_path;   // Path to libtashkeel ORT model
+
+        std::filesystem::path m_model_onnx_path;                      // Path to ONNX voice file (see https://github.com/mush42/libtashkeel/) 
+        std::filesystem::path m_model_onnx_json_path;                 // Path to JSON voice config file
+        std::filesystem::path m_espeak_data_path;                     // Path to espeak-ng-data folder
+
+        bool m_use_espeak = true;
+        bool m_use_tashkeel = false;
+    };
+
+    // Audio synthesis parameters
+    struct SSynthesisConfig {
+        std::optional<float> m_noise_scale = 0.667f;                 // Audio generator noise (default = 0.667f)
+        std::optional<float> m_length_scale = 1.0f;                  // Speaking speed (<1.0f = faster, >1.0f = slower, 1.0f == normal/default)
+        std::optional<float> m_noise_width = 0.8f;                   // Phoneme width noise variation (default = 0.8f)
+        std::optional<float> m_sentence_silence_seconds = 0.2f;      // Silence after each sentence (default = 0.2f)
+    };
+
+    // Hardware
+    // Note: Could possibly use a boolean
+    enum class EGPUBackendType : uint8_t {
+        None = 0,
+
+        // GPU acceleration options
+        // TODO: Investigate other GPU acceleration options included for Piper
+        Cuda = 1,
+
+        Default = None
+    };
+
+    struct SHardwareConfig {
+        EGPUBackendType m_gpu_acceleration_type = EGPUBackendType::Default;
+    };
+
+    //////////////////////////////////////////////////////
+
+    // Top-level config struct for interfacing with Piper
+    struct VB_TTS_API STTSConfig {
+        SVoiceModelConfig   m_voice_config;
+        SSynthesisConfig    m_synthesis_config;
+        SHardwareConfig     m_hardware_config;
+    };
+}
