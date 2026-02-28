e5fcb9ef26d4c6b95f272253f2f4ebbb03e706a8
cpap7 <mjajarmi2000@outlook.com>
2026-02-28 18:25:31 -0500

    Updated premake script for VoxBox-WhisperAPI

--- VoxBox-WhisperAPI/premake5.lua
+++ VoxBox-WhisperAPI/premake5.lua
@@ -3,9 +3,10 @@ project "VoxBox-WhisperAPI"
     kind "SharedLib"
     language "C++"
     cppdialect "C++20"
-
+    staticruntime "Off" -- /MD 
     targetdir ("bin/%{cfg.buildcfg}-%{cfg.architecture}")
     objdir ("bin-int/%{cfg.buildcfg}-%{cfg.architecture}")
+
 
     -- Precompiled header
     pchheader "vbwpch.h"
@@ -18,7 +19,7 @@ project "VoxBox-WhisperAPI"
 
     includedirs {
         "Source",                                    -- For vbwpch.h
-        "../VoxBox-Shared",                          -- For useful, global utilities like PlatformDetection.h
+        "../VoxBox-Shared",                          -- For global utilities
         "Vendor/whisper.cpp/include",                -- whisper.h
         "Vendor/whisper.cpp/ggml/include",           -- ggml headers
     }
@@ -28,6 +29,8 @@ project "VoxBox-WhisperAPI"
         "Vendor/whisper.cpp/%{cfg.buildcfg:lower()}/ggml/src/%{cfg.buildcfg}",                 -- ggml.lib
         "Vendor/whisper.cpp/%{cfg.buildcfg:lower()}/ggml/src/ggml-base/%{cfg.buildcfg}",       -- ggml-base.lib
         "Vendor/whisper.cpp/%{cfg.buildcfg:lower()}/ggml/src/ggml-cpu/%{cfg.buildcfg}",        -- ggml-cpu.lib
+        "Vendor/whisper.cpp/%{cfg.buildcfg:lower()}/ggml/src/ggml-vulkan/%{cfg.buildcfg}",     -- ggml-vulkan.lib
+
     }
 
     links {
@@ -35,23 +38,33 @@ project "VoxBox-WhisperAPI"
         "ggml",
         "ggml-base",
         "ggml-cpu",
+        "ggml-vulkan",
+        "%{Library.Vulkan}"
     }
 
     filter "system:windows"
         systemversion "latest"
-        characterset "Unicode"
+        toolset "v145" -- VS 2026
+        buildoptions { "/utf-8", "/Wall" }
         defines { 
             "_WINDOWS",
             "_USRDLL",
             "VB_EXPORT_STT" -- Export dll symbols
         }
+        
 
     filter "configurations:Debug"
         runtime "Debug"
         symbols "On"
         defines { "_DEBUG" }
+        links {
+            "msvcprtd" -- To prevent LNK2019 errors from the regex & algorithm headers that whisper uses
+        }
 
     filter "configurations:Release"
         runtime "Release"
         optimize "On"
         defines { "NDEBUG" }
+        links {
+            "msvcprt" -- To prevent LNK2019 errors from the regex & algorithm headers that whisper uses
+        }
