2b4ca1164e1bd99bc44add0b19aaf3098597db02
cpap7 <mjajarmi2000@outlook.com>
2026-02-28 18:21:47 -0500

    Changed VoxBox-PiperApp project to VoxBox-TestApp. Moved all relevant files to TestApp folder.

--- VoxBox-TestApp/premake5.lua
+++ VoxBox-TestApp/premake5.lua
@@ -1,5 +1,5 @@
 -- premake5.lua
-project "VoxBox-PiperApp"
+project "VoxBox-TestApp"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++20"
@@ -16,28 +16,37 @@ project "VoxBox-PiperApp"
 
     includedirs {
         "../VoxBox-PiperAPI/Source/API",        -- For VoxBoxTTS.h
+        "../VoxBox-WhisperAPI/Source/API",      -- For VoxBoxSTT.h
         "../VoxBox-Common/Source",              -- For VBCommon.h
     }
 
     libdirs {
         "../VoxBox-PiperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}",  -- VoxBox-Piper.lib
+        "../VoxBox-WhisperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}",  -- VoxBox-WhisperAPI.lib
+
     }
 
     links {
         "VoxBox-PiperAPI",
+        "VoxBox-WhisperAPI",
     }
 
     postbuildcommands {
+        -- Piper
         '{COPYDIR} "../VoxBox-PiperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}/espeak" "%{cfg.targetdir}"',
         '{COPYFILE} "../VoxBox-PiperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}/espeak-ng.dll" "%{cfg.targetdir}"',
         '{COPYFILE} "../VoxBox-PiperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}/onnxruntime.dll" "%{cfg.targetdir}"',
         '{COPYFILE} "../VoxBox-PiperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}/VoxBox-PiperAPI.dll" "%{cfg.targetdir}"',
         '{COPYFILE} "../VoxBox-PiperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}/piper_phonemize.dll" "%{cfg.targetdir}"',
         '{COPYFILE} "../VoxBox-PiperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}/piper.exe" "%{cfg.targetdir}"',
+        -- Whisper        
+        '{COPYFILE} "../VoxBox-WhisperAPI/bin/%{cfg.buildcfg}-%{cfg.architecture}/VoxBox-WhisperAPI.dll" "%{cfg.targetdir}"',
+
     }
 
     filter "system:windows"
         systemversion "latest"
+        toolset "v145" -- VS 2026
         defines { "_WINDOWS" }
 
     filter "configurations:Debug"
